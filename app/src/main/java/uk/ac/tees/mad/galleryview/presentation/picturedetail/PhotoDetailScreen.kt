d3bd3fbe7ee9219d97c557523fb4fcb09b1179cf
nikhilobelineni <dinesh.papaka94@gmail.com>
2024-12-31 06:50:05 +0530

    commit-14

--- app/src/main/java/uk/ac/tees/mad/galleryview/presentation/picturedetail/PhotoDetailScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/galleryview/presentation/picturedetail/PhotoDetailScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -19,8 +20,12 @@ import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,6 +40,7 @@ import coil.compose.rememberAsyncImagePainter
 import uk.ac.tees.mad.galleryview.presentation.galleryview.ImageData
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PhotoDetailScreen(
     imageDetail: ImageData,
@@ -42,83 +48,75 @@ fun PhotoDetailScreen(
 ) {
     val context = LocalContext.current
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(300.dp),
-            shape = RoundedCornerShape(8.dp),
-            colors = CardDefaults.cardColors(Color.LightGray)
-        ) {
-            Image(
-                painter = rememberAsyncImagePainter(imageDetail.imageUrl),
-                contentDescription = "Detailed Image",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "") },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            val shareIntent = Intent().apply {
+                                action = Intent.ACTION_SEND
+                                putExtra(Intent.EXTRA_TEXT, imageDetail.imageUrl)
+                                type = "text/plain"
+                            }
+                            context.startActivity(
+                                Intent.createChooser(shareIntent, "Share image via")
+                            )
+                        }
+                    ) {
+                        Icon(imageVector = Icons.Default.Share, contentDescription = "share")
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Default.Delete, contentDescription = "delete")
+                    }
+                }
             )
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Image description
-        Text(
-            text = imageDetail.description,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-
-        // Tags
-        Text(
-            text = "Tags: ${imageDetail.tags.joinToString(", ")}",
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Action Buttons
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            // Share button
-            Button(
-                onClick = {
-                    val shareIntent = Intent().apply {
-                        action = Intent.ACTION_SEND
-                        putExtra(Intent.EXTRA_TEXT, imageDetail.imageUrl)
-                        type = "text/plain"
-                    }
-                    context.startActivity(
-                        Intent.createChooser(shareIntent, "Share image via")
+    ) { paddingValues ->
+        Box(modifier = Modifier.padding(paddingValues)) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = CardDefaults.cardColors(Color.LightGray)
+                ) {
+                    Image(
+                        painter = rememberAsyncImagePainter(imageDetail.imageUrl),
+                        contentDescription = "Detailed Image",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
                     )
                 }
-            ) {
-                Icon(Icons.Default.Share, contentDescription = "Share")
-                Spacer(modifier = Modifier.width(8.dp))
-                Text("Share")
-            }
 
-            // Delete button
-            Button(
-                onClick = {
+                Spacer(modifier = Modifier.height(16.dp))
 
-                }
-            ) {
-                Icon(Icons.Default.Delete, contentDescription = "Delete")
-                Spacer(modifier = Modifier.width(8.dp))
-                Text("Delete")
+                // Image description
+                Text(
+                    text = imageDetail.description,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                )
+
+                Text(
+                    text = "Tags: ${imageDetail.tags.joinToString(", ")}",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                )
+
             }
         }
     }
